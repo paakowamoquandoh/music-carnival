6679223d81a74b2876d20c6cfd7798963a81622c
Felix Ekow Amoquandoh <105642899+felixEKow@users.noreply.github.com>
2023-02-14 12:59:07 +0000

    app update

--- src/js/login.js
+++ src/js/login.js
@@ -3,6 +3,10 @@ const signUpBtn = document.getElementById("signUp");
 const fistForm = document.getElementById("form1");
 const secondForm = document.getElementById("form2");
 const container = document.querySelector(".container");
+let signInForm = document.getElementById("signInFormMain");
+let email = document.getElementById("email");
+let passWord = document.getElementById("password");
+let errorMessage = document.getElementsByClassName("errorMsg");
 
 signInBtn.addEventListener("click", () => {
 	container.classList.remove("right-panel-active");
@@ -14,3 +18,23 @@ signUpBtn.addEventListener("click", () => {
 
 fistForm.addEventListener("submit", (e) => e.preventDefault());
 secondForm.addEventListener("submit", (e) => e.preventDefault());
+
+
+
+let formEngine = function (id, number, message){
+    if(id.value.trim() === ""){
+        errorMessage[number].innerHTML = message;    
+    } else{
+        errorMessage[number].innerHTML = "";     
+    }
+}
+
+signInForm.addEventListener("submit", function(e){
+   
+    if (formEngine) {
+		window.location.assign("../payment.html")
+	} else {
+	formEngine(email, 0, "Email should not be blank");
+    formEngine(passWord, 1, "Password must contain letter and numbers");
+	}
+})
