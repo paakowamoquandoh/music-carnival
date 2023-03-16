22529acee9ac4df99f3843b348667c84d71d8cd7
Felix Ekow Amoquandoh <paakowamoquandoh@gmail.com>
2023-03-16 18:04:38 +0000

    index page updates

--- src/js/script.js
+++ src/js/script.js
@@ -32,3 +32,38 @@ let navbody = selectedElement("body");
 menuToggler.addEventListener("click", function(){
     navbody.classList.toggle("open")
 })
+
+
+
+// scroll Animations
+window.sr = ScrollReveal();
+
+sr.reveal('.animateLeft', {
+    origin: 'left',
+    duration: 1000,
+    distance: '25rem',
+    delay: 300
+});
+
+sr.reveal('.animateRight', {
+    origin: 'right',
+    duration: 1000,
+    distance: '25rem',
+    delay: 600
+});
+
+sr.reveal('.animateTop', {
+    origin: 'top',
+    duration: 1000,
+    distance: '25rem',
+    delay: 600
+});
+
+sr.reveal('.animateBottom', {
+    origin: 'bottom',
+    duration: 1000,
+    distance: '25rem',
+    delay: 600
+});
+
+
