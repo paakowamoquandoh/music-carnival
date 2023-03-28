4bc00791543d14d5c3ed36735569194a5ca459d5
Felix Ekow Amoquandoh <paakowamoquandoh@gmail.com>
2023-03-28 00:53:32 +0000

    dist page update

--- src/js/dist.js
+++ src/js/dist.js
@@ -11,6 +11,9 @@ const DOMstrings = {
   stepNextBtnClass: 'js-btn-next'
 };
 
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+
 //remove class from a set of items
 const removeClasses = (elemSet, className) => {
   
@@ -177,8 +180,47 @@ const setAnimationType = (newType) => {
 //selector onchange - changing animation
 const animationSelect = document.querySelector('.pick-animation__select');
 
-animationSelect.addEventListener('change', () => {
-  const newAnimationType = animationSelect.value;
+// animationSelect.addEventListener('change', () => {
+//   const newAnimationType = animationSelect.value;
   
-  setAnimationType(newAnimationType);
+//   setAnimationType(newAnimationType);
+// });
+
+
+// Switch theme/add to local storage
+let body = document.body;
+const themeToggleBtn = body.querySelector('#theme-change-button');
+const currentTheme = localStorage.getItem('currentTheme');
+
+// Check to see if there is a theme preference in local Storage, if so add the ligt theme to the body
+if (currentTheme) {
+    body.classList.add('light-theme');
+}
+
+themeToggleBtn.addEventListener('click', function () {
+    // Add light theme on click
+    body.classList.toggle('light-theme');
+
+    // If the body has the class of light theme then add it to local Storage, if not remove it
+    if (body.classList.contains('light-theme')) {
+        localStorage.setItem('currentTheme', 'themeActive');
+    } else {
+        localStorage.removeItem('currentTheme');
+    }
 });
+
+
+// menu open and close
+const selectedElement = function(element){
+    return document.querySelector(element);
+};
+
+let menuToggler = selectedElement(".menuToggle");
+let navbody = selectedElement("body");
+
+menuToggler.addEventListener("click", function(){
+    navbody.classList.toggle("open")
+})
+
+
+
